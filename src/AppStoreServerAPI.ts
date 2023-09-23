0eee81a2e95adb10d9a739dd423399adc5dc859f
Desttro <bozstvi-mluvil0a@icloud.com>
2023-09-23 18:56:00 +0200

    Refactor library for Cloudflare Workers compatibility
    - Removed Node.js-specific dependencies.
    - Integrated with Web Crypto API for cross-environment compatibility.
    - Ensured library functions seamlessly in Cloudflare Workers.

--- src/AppStoreServerAPI.ts
+++ src/AppStoreServerAPI.ts
@@ -1,238 +1,272 @@
-import fetch from "node-fetch"
-import { v4 as uuidv4 } from "uuid"
-import * as jose from "jose"
+import * as jose from "jose";
+import { v4 as uuidv4 } from "uuid";
+import { AppStoreError } from "./Errors";
 import {
-  CheckTestNotificationResponse,
-  Environment,
-  HistoryResponse,
-  NotificationHistoryQuery,
-  NotificationHistoryRequest,
-  NotificationHistoryResponse,
-  OrderLookupResponse,
-  SendTestNotificationResponse,
-  StatusResponse,
-  SubscriptionStatusesQuery,
-  TransactionHistoryQuery,
-  TransactionInfoResponse,
-  ExtendRenewalDateResponse,
-  ExtendRenewalDateRequest
-} from "./Models"
-import { AppStoreError } from "./Errors"
+	CheckTestNotificationResponse,
+	Environment,
+	ExtendRenewalDateRequest,
+	ExtendRenewalDateResponse,
+	HistoryResponse,
+	NotificationHistoryQuery,
+	NotificationHistoryRequest,
+	NotificationHistoryResponse,
+	OrderLookupResponse,
+	SendTestNotificationResponse,
+	StatusResponse,
+	SubscriptionStatusesQuery,
+	TransactionHistoryQuery,
+	TransactionInfoResponse,
+} from "./Models";
 
-type HTTPMethod = "GET" | "POST" | "PUT"
+type HTTPMethod = "GET" | "POST" | "PUT";
 
 interface QueryConvertible {
-  [key: string]: string | number | boolean | number[]
+	[key: string]: string | number | boolean | number[];
 }
 
 export class AppStoreServerAPI {
-  // The maximum age that an authentication token is allowed to have, as decided by Apple.
-  static readonly maxTokenAge: number = 3600 // seconds, = 1 hour
+	// The maximum age that an authentication token is allowed to have, as decided by Apple.
+	static readonly maxTokenAge: number = 3600; // seconds, = 1 hour
 
-  readonly environment: Environment
-  private readonly baseUrl: string
+	readonly environment: Environment;
+	private readonly baseUrl: string;
 
-  private readonly key: Promise<jose.KeyLike>
-  private readonly keyId: string
-  private readonly issuerId: string
-  private readonly bundleId: string
-  private token?: string
-  private tokenExpiry: Date = new Date(0)
+	private readonly key: Promise<jose.KeyLike>;
+	private readonly keyId: string;
+	private readonly issuerId: string;
+	private readonly bundleId: string;
+	private token?: string;
+	private tokenExpiry: Date = new Date(0);
 
-  /**
-   * @param key the key downloaded from App Store Connect in PEM-encoded PKCS8 format.
-   * @param keyId the id of the key, retrieved from App Store Connect
-   * @param issuerId your issuer ID, retrieved from App Store Connect
-   * @param bundleId bundle ID of your app
-   */
-  constructor(key: string, keyId: string, issuerId: string, bundleId: string, environment = Environment.Production) {
-    this.key = jose.importPKCS8(key, "ES256")
-    this.keyId = keyId
-    this.issuerId = issuerId
-    this.bundleId = bundleId
-    this.environment = environment
+	/**
+	 * @param key the key downloaded from App Store Connect in PEM-encoded PKCS8 format.
+	 * @param keyId the id of the key, retrieved from App Store Connect
+	 * @param issuerId your issuer ID, retrieved from App Store Connect
+	 * @param bundleId bundle ID of your app
+	 */
+	constructor(
+		key: string,
+		keyId: string,
+		issuerId: string,
+		bundleId: string,
+		environment = Environment.Production,
+	) {
+		this.key = jose.importPKCS8(key, "ES256");
+		this.keyId = keyId;
+		this.issuerId = issuerId;
+		this.bundleId = bundleId;
+		this.environment = environment;
 
-    if (environment === Environment.Sandbox) {
-      this.baseUrl = "https://api.storekit-sandbox.itunes.apple.com"
-    } else {
-      this.baseUrl = "https://api.storekit.itunes.apple.com"
-    }
-  }
+		if (environment === Environment.Sandbox) {
+			this.baseUrl = "https://api.storekit-sandbox.itunes.apple.com";
+		} else {
+			this.baseUrl = "https://api.storekit.itunes.apple.com";
+		}
+	}
 
-  // API Endpoints
+	// API Endpoints
 
-  /**
-   * https://developer.apple.com/documentation/appstoreserverapi/get_transaction_history
-   */
-  async getTransactionHistory(transactionId: string, query: TransactionHistoryQuery = {}): Promise<HistoryResponse> {
-    const path = this.addQuery(`/inApps/v1/history/${transactionId}`, { ...query })
-    return this.makeRequest("GET", path)
-  }
+	/**
+	 * https://developer.apple.com/documentation/appstoreserverapi/get_transaction_history
+	 */
+	async getTransactionHistory(
+		transactionId: string,
+		query: TransactionHistoryQuery = {},
+	): Promise<HistoryResponse> {
+		const path = this.addQuery(`/inApps/v1/history/${transactionId}`, {
+			...query,
+		});
+		return this.makeRequest("GET", path);
+	}
 
-  /**
-   * https://developer.apple.com/documentation/appstoreserverapi/get_transaction_info
-   */
-  async getTransactionInfo(transactionId: string): Promise<TransactionInfoResponse> {
-    return this.makeRequest("GET", `/inApps/v1/transactions/${transactionId}`)
-  }
+	/**
+	 * https://developer.apple.com/documentation/appstoreserverapi/get_transaction_info
+	 */
+	async getTransactionInfo(
+		transactionId: string,
+	): Promise<TransactionInfoResponse> {
+		return this.makeRequest("GET", `/inApps/v1/transactions/${transactionId}`);
+	}
 
-  /**
-   * https://developer.apple.com/documentation/appstoreserverapi/get_all_subscription_statuses
-   */
-  async getSubscriptionStatuses(transactionId: string, query: SubscriptionStatusesQuery = {}): Promise<StatusResponse> {
-    const path = this.addQuery(`/inApps/v1/subscriptions/${transactionId}`, { ...query })
-    return this.makeRequest("GET", path)
-  }
+	/**
+	 * https://developer.apple.com/documentation/appstoreserverapi/get_all_subscription_statuses
+	 */
+	async getSubscriptionStatuses(
+		transactionId: string,
+		query: SubscriptionStatusesQuery = {},
+	): Promise<StatusResponse> {
+		const path = this.addQuery(`/inApps/v1/subscriptions/${transactionId}`, {
+			...query,
+		});
+		return this.makeRequest("GET", path);
+	}
 
-  /**
-   * https://developer.apple.com/documentation/appstoreserverapi/look_up_order_id
-   */
-  async lookupOrder(orderId: string): Promise<OrderLookupResponse> {
-    return this.makeRequest("GET", `/inApps/v1/lookup/${orderId}`)
-  }
+	/**
+	 * https://developer.apple.com/documentation/appstoreserverapi/look_up_order_id
+	 */
+	async lookupOrder(orderId: string): Promise<OrderLookupResponse> {
+		return this.makeRequest("GET", `/inApps/v1/lookup/${orderId}`);
+	}
 
-  /**
-   * https://developer.apple.com/documentation/appstoreserverapi/extend_a_subscription_renewal_date
-   */
-  async extendSubscriptionRenewalDate(
-    originalTransactionId: string,
-    request: ExtendRenewalDateRequest
-  ): Promise<ExtendRenewalDateResponse> {
-    return this.makeRequest("PUT", `/inApps/v1/subscriptions/extend/${originalTransactionId}`, request)
-  }
+	/**
+	 * https://developer.apple.com/documentation/appstoreserverapi/extend_a_subscription_renewal_date
+	 */
+	async extendSubscriptionRenewalDate(
+		originalTransactionId: string,
+		request: ExtendRenewalDateRequest,
+	): Promise<ExtendRenewalDateResponse> {
+		return this.makeRequest(
+			"PUT",
+			`/inApps/v1/subscriptions/extend/${originalTransactionId}`,
+			request,
+		);
+	}
 
-  /**
-   * https://developer.apple.com/documentation/appstoreserverapi/request_a_test_notification
-   */
-  async requestTestNotification(): Promise<SendTestNotificationResponse> {
-    return this.makeRequest("POST", "/inApps/v1/notifications/test")
-  }
+	/**
+	 * https://developer.apple.com/documentation/appstoreserverapi/request_a_test_notification
+	 */
+	async requestTestNotification(): Promise<SendTestNotificationResponse> {
+		return this.makeRequest("POST", "/inApps/v1/notifications/test");
+	}
 
-  /**
-   * https://developer.apple.com/documentation/appstoreserverapi/get_test_notification_status
-   */
-  async getTestNotificationStatus(id: string): Promise<CheckTestNotificationResponse> {
-    return this.makeRequest("GET", `/inApps/v1/notifications/test/${id}`)
-  }
+	/**
+	 * https://developer.apple.com/documentation/appstoreserverapi/get_test_notification_status
+	 */
+	async getTestNotificationStatus(
+		id: string,
+	): Promise<CheckTestNotificationResponse> {
+		return this.makeRequest("GET", `/inApps/v1/notifications/test/${id}`);
+	}
 
-  /**
-   * https://developer.apple.com/documentation/appstoreserverapi/get_notification_history
-   */
-  async getNotificationHistory(
-    request: NotificationHistoryRequest,
-    query: NotificationHistoryQuery = {}
-  ): Promise<NotificationHistoryResponse> {
-    const path = this.addQuery("/inApps/v1/notifications/history", { ...query })
-    return this.makeRequest("POST", path, request)
-  }
+	/**
+	 * https://developer.apple.com/documentation/appstoreserverapi/get_notification_history
+	 */
+	async getNotificationHistory(
+		request: NotificationHistoryRequest,
+		query: NotificationHistoryQuery = {},
+	): Promise<NotificationHistoryResponse> {
+		const path = this.addQuery("/inApps/v1/notifications/history", {
+			...query,
+		});
+		return this.makeRequest("POST", path, request);
+	}
 
-  /**
-   * Performs a network request against the API and handles the result.
-   */
-  private async makeRequest(method: HTTPMethod, path: string, body?: any): Promise<any> {
-    const token = await this.getToken()
-    const url = this.baseUrl + path
-    const serializedBody = body ? JSON.stringify(body) : undefined
+	/**
+	 * Performs a network request against the API and handles the result.
+	 */
+	private async makeRequest(
+		method: HTTPMethod,
+		path: string,
+		body?: any,
+	): Promise<any> {
+		const token = await this.getToken();
+		const url = this.baseUrl + path;
+		const serializedBody = body ? JSON.stringify(body) : undefined;
 
-    const result = await fetch(url, {
-      method: method,
-      body: serializedBody,
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json"
-      }
-    })
+		const result = await fetch(url, {
+			method: method,
+			body: serializedBody,
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+		});
 
-    if (result.status === 200) {
-      return result.json()
-    }
+		if (result.status === 200) {
+			return result.json();
+		}
 
-    switch (result.status) {
-      case 400:
-      case 403:
-      case 404:
-      case 429:
-      case 500:
-        const body = await result.json()
-        throw new AppStoreError(body.errorCode, body.errorMessage)
+		switch (result.status) {
+			case 400:
+			case 403:
+			case 404:
+			case 429:
+			case 500: {
+				const body = await result.json();
+				throw new AppStoreError(body.errorCode, body.errorMessage);
+			}
 
-      case 401:
-        this.token = undefined
-        throw new Error("The request is unauthorized; the JSON Web Token (JWT) is invalid.")
+			case 401:
+				this.token = undefined;
+				throw new Error(
+					"The request is unauthorized; the JSON Web Token (JWT) is invalid.",
+				);
 
-      default:
-        throw new Error("An unknown error occurred")
-    }
-  }
+			default:
+				throw new Error("An unknown error occurred");
+		}
+	}
 
-  /**
-   * Returns an existing authentication token (if its still valid) or generates a new one.
-   */
-  private async getToken(): Promise<string> {
-    // Reuse previously created token if it hasn't expired.
-    if (this.token && !this.tokenExpired) return this.token
+	/**
+	 * Returns an existing authentication token (if its still valid) or generates a new one.
+	 */
+	private async getToken(): Promise<string> {
+		// Reuse previously created token if it hasn't expired.
+		if (this.token && !this.tokenExpired) return this.token;
 
-    // Tokens must expire after at most 1 hour.
-    const now = new Date()
-    const expiry = new Date(now.getTime() + AppStoreServerAPI.maxTokenAge * 1000)
-    const expirySeconds = Math.floor(expiry.getTime() / 1000)
+		// Tokens must expire after at most 1 hour.
+		const now = new Date();
+		const expiry = new Date(
+			now.getTime() + AppStoreServerAPI.maxTokenAge * 1000,
+		);
+		const expirySeconds = Math.floor(expiry.getTime() / 1000);
 
-    const payload = {
-      bid: this.bundleId,
-      nonce: uuidv4()
-    }
+		const payload = {
+			bid: this.bundleId,
+			nonce: uuidv4(),
+		};
 
-    const privateKey = await this.key
+		const privateKey = await this.key;
 
-    const jwt = await new jose.SignJWT(payload)
-      .setProtectedHeader({ alg: "ES256", kid: this.keyId, typ: "JWT" })
-      .setIssuer(this.issuerId)
-      .setIssuedAt()
-      .setExpirationTime(expirySeconds)
-      .setAudience("appstoreconnect-v1")
-      .sign(privateKey)
+		const jwt = await new jose.SignJWT(payload)
+			.setProtectedHeader({ alg: "ES256", kid: this.keyId, typ: "JWT" })
+			.setIssuer(this.issuerId)
+			.setIssuedAt()
+			.setExpirationTime(expirySeconds)
+			.setAudience("appstoreconnect-v1")
+			.sign(privateKey);
 
-    this.token = jwt
-    this.tokenExpiry = expiry
+		this.token = jwt;
+		this.tokenExpiry = expiry;
 
-    return jwt
-  }
+		return jwt;
+	}
 
-  /**
-   * Returns whether the previously generated token can still be used.
-   */
-  private get tokenExpired(): boolean {
-    // We consider the token to be expired slightly before it actually is to allow for some networking latency.
-    const headroom = 60 // seconds
-    const now = new Date()
-    const cutoff = new Date(now.getTime() - headroom * 1000)
+	/**
+	 * Returns whether the previously generated token can still be used.
+	 */
+	private get tokenExpired(): boolean {
+		// We consider the token to be expired slightly before it actually is to allow for some networking latency.
+		const headroom = 60; // seconds
+		const now = new Date();
+		const cutoff = new Date(now.getTime() - headroom * 1000);
 
-    return !this.tokenExpiry || this.tokenExpiry < cutoff
-  }
+		return !this.tokenExpiry || this.tokenExpiry < cutoff;
+	}
 
-  /**
-   * Serializes a query object into a query string and appends it
-   * the provided path.
-   */
-  private addQuery(path: string, query: QueryConvertible): string {
-    const params = new URLSearchParams()
+	/**
+	 * Serializes a query object into a query string and appends it
+	 * the provided path.
+	 */
+	private addQuery(path: string, query: QueryConvertible): string {
+		const params = new URLSearchParams();
 
-    for (const [key, value] of Object.entries(query)) {
-      if (Array.isArray(value)) {
-        for (const item of value) {
-          params.append(key, item.toString())
-        }
-      } else {
-        params.set(key, value.toString())
-      }
-    }
+		for (const [key, value] of Object.entries(query)) {
+			if (Array.isArray(value)) {
+				for (const item of value) {
+					params.append(key, item.toString());
+				}
+			} else {
+				params.set(key, value.toString());
+			}
+		}
 
-    const queryString = params.toString()
+		const queryString = params.toString();
 
-    if (queryString === "") {
-      return path
-    } else {
-      return `${path}?${queryString}`
-    }
-  }
+		if (queryString === "") {
+			return path;
+		} else {
+			return `${path}?${queryString}`;
+		}
+	}
 }
--- src/Decoding.ts
+++ src/Decoding.ts
@@ -1,41 +1,124 @@
-import { X509Certificate } from "crypto"
-import * as jose from "jose"
-import { APPLE_ROOT_CA_G3_FINGERPRINT } from "./AppleRootCertificate"
-import { CertificateValidationError } from "./Errors"
+import * as jose from "jose";
+import { Certificate, CertificateChainValidationEngine } from "pkijs";
+import { BufferSourceConverter } from "pvtsutils";
+import { Convert } from "pvtsutils";
+import { stringToArrayBuffer } from "pvutils";
+
+import { APPLE_ROOT_CA_G3_FINGERPRINT } from "./AppleRootCertificate";
+import { CertificateValidationError } from "./Errors";
 import {
-  DecodedNotificationPayload,
-  JWSRenewalInfo,
-  JWSRenewalInfoDecodedPayload,
-  JWSTransaction,
-  JWSTransactionDecodedPayload
-} from "./Models"
+	DecodedNotificationPayload,
+	JWSRenewalInfo,
+	JWSRenewalInfoDecodedPayload,
+	JWSTransaction,
+	JWSTransactionDecodedPayload,
+} from "./Models";
 
 export async function decodeTransactions(
-  signedTransactions: JWSTransaction[],
-  rootCertFingerprint?: string
+	signedTransactions: JWSTransaction[],
+	rootCertFingerprint?: string,
 ): Promise<JWSTransactionDecodedPayload[]> {
-  return Promise.all(signedTransactions.map(transaction => decodeJWS(transaction, rootCertFingerprint)))
+	return Promise.all(
+		signedTransactions.map((transaction) =>
+			decodeJWS(transaction, rootCertFingerprint),
+		),
+	);
 }
 
 export async function decodeTransaction(
-  transaction: JWSTransaction,
-  rootCertFingerprint?: string
+	transaction: JWSTransaction,
+	rootCertFingerprint?: string,
 ): Promise<JWSTransactionDecodedPayload> {
-  return decodeJWS(transaction, rootCertFingerprint)
+	return decodeJWS(transaction, rootCertFingerprint);
 }
 
 export async function decodeRenewalInfo(
-  info: JWSRenewalInfo,
-  rootCertFingerprint?: string
+	info: JWSRenewalInfo,
+	rootCertFingerprint?: string,
 ): Promise<JWSRenewalInfoDecodedPayload> {
-  return decodeJWS(info, rootCertFingerprint)
+	return decodeJWS(info, rootCertFingerprint);
 }
 
 export async function decodeNotificationPayload(
-  payload: string,
-  rootCertFingerprint?: string
+	payload: string,
+	rootCertFingerprint?: string,
 ): Promise<DecodedNotificationPayload> {
-  return decodeJWS(payload, rootCertFingerprint)
+	return decodeJWS(payload, rootCertFingerprint);
+}
+
+// Compute the SHA-256 hash using the Web Cryptography API
+async function computeSHA256(arrayBuffer: ArrayBuffer) {
+	const hashBuffer = await crypto.subtle.digest("SHA-256", arrayBuffer);
+	return new Uint8Array(hashBuffer);
+}
+
+// Convert the hash to colon-separated hexadecimal
+function bufferToColonSeparatedHex(buffer: Uint8Array) {
+	return Array.prototype.map
+		.call(buffer, (byte) =>
+			// biome-ignore lint/style/useTemplate: use of template literals in this context doesn't make the code significantly clearer.
+			("00" + byte.toString(16))
+				.slice(-2)
+				.toUpperCase(),
+		)
+		.join(":");
+}
+
+/**
+ * Decodes a PEM (Privacy-Enhanced Mail) string into an array of ArrayBuffers.
+ * @param pem The PEM string to decode.
+ * @param tag The tag to use for the regular expression pattern. Defaults to "[A-Z0-9 ]+".
+ * @returns An array of ArrayBuffers.
+ */
+function decodePEM(pem: string, tag = "[A-Z0-9 ]+"): ArrayBuffer[] {
+	// Create a regular expression pattern to match the PEM string.
+	const pattern = new RegExp(
+		`-{5}BEGIN ${tag}-{5}([a-zA-Z0-9=+\\/\\n\\r]+)-{5}END ${tag}-{5}`,
+		"g",
+	);
+
+	const res: ArrayBuffer[] = [];
+	let matches: RegExpExecArray | null = null;
+	// Loop through the matches in the PEM string.
+	// biome-ignore lint/suspicious/noAssignInExpressions: This is the only way to loop through the matches.
+	while ((matches = pattern.exec(pem))) {
+		// Remove carriage returns and newlines from the base64 string.
+		const base64 = matches[1].replace(/\r/g, "").replace(/\n/g, "");
+		// Convert the base64 string to an ArrayBuffer and add it to the result array.
+		res.push(Convert.FromBase64(base64));
+	}
+
+	// Return the result array.
+	return res;
+}
+
+/**
+ * Parses a certificate from a BufferSource.
+ * @param source The BufferSource to parse.
+ * @returns An array of Certificates.
+ */
+export function parseCertificate(source: BufferSource): Certificate[] {
+	const buffers: ArrayBuffer[] = [];
+
+	// Convert the BufferSource to an ArrayBuffer.
+	const buffer = BufferSourceConverter.toArrayBuffer(source);
+	// Convert the ArrayBuffer to a binary string.
+	const pem = Convert.ToBinary(buffer);
+	// If the binary string is a PEM string, decode it. Otherwise, use the original ArrayBuffer.
+	if (/----BEGIN CERTIFICATE-----/.test(pem)) {
+		buffers.push(...decodePEM(pem, "CERTIFICATE"));
+	} else {
+		buffers.push(buffer);
+	}
+
+	const res: Certificate[] = [];
+	// Convert each ArrayBuffer in the array to a Certificate and add it to the result array.
+	for (const item of buffers) {
+		res.push(Certificate.fromBER(item));
+	}
+
+	// Return the result array.
+	return res;
 }
 
 /**
@@ -44,26 +127,32 @@ export async function decodeNotificationPayload(
  * @param token JWS token
  * @param rootCertFingerprint Root certificate to validate against. Defaults to Apple's G3 CA but can be overriden for testing purposes.
  */
-async function decodeJWS(token: string, rootCertFingerprint: string = APPLE_ROOT_CA_G3_FINGERPRINT): Promise<any> {
-  // Extracts the key used to sign the JWS from the header of the token
-  const getKey: jose.CompactVerifyGetKey = async (protectedHeader, _token) => {
-    // RC 7515 stipulates that the key used to sign the JWS must be the first in the chain.
-    // https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.6
+async function decodeJWS(
+	token: string,
+	rootCertFingerprint: string = APPLE_ROOT_CA_G3_FINGERPRINT,
+): Promise<any> {
+	// Extracts the key used to sign the JWS from the header of the token
+	const getKey: jose.CompactVerifyGetKey = async (protectedHeader, _token) => {
+		// RC 7515 stipulates that the key used to sign the JWS must be the first in the chain.
+		// https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.6
 
-    // jose will not import the certificate unless it is in a proper PKCS8 format.
-    const certs = protectedHeader.x5c?.map(c => `-----BEGIN CERTIFICATE-----\n${c}\n-----END CERTIFICATE-----`) ?? []
+		// jose will not import the certificate unless it is in a proper PKCS8 format.
+		const certs =
+			protectedHeader.x5c?.map(
+				(c) => `-----BEGIN CERTIFICATE-----\n${c}\n-----END CERTIFICATE-----`,
+			) ?? [];
 
-    validateCertificates(certs, rootCertFingerprint)
+		await validateCertificates(certs, rootCertFingerprint);
 
-    return jose.importX509(certs[0], "ES256")
-  }
+		return jose.importX509(certs[0], "ES256");
+	};
 
-  const { payload } = await jose.compactVerify(token, getKey)
+	const { payload } = await jose.compactVerify(token, getKey);
 
-  const decoded = new TextDecoder().decode(payload)
-  const json = JSON.parse(decoded)
+	const decoded = new TextDecoder().decode(payload);
+	const json = JSON.parse(decoded);
 
-  return json
+	return json;
 }
 
 /**
@@ -73,30 +162,45 @@ async function decodeJWS(token: string, rootCertFingerprint: string = APPLE_ROOT
  * @param rootCertFingerprint Expected SHA256 signature of the root certificate
  * @throws {CertificateValidationError} if any of the validation checks fail
  */
-function validateCertificates(certificates: string[], rootCertFingerprint: string) {
-  if (certificates.length === 0) throw new CertificateValidationError([])
+async function validateCertificates(
+	certificates: string[],
+	rootCertFingerprint: string,
+) {
+	// If no certificates are provided, throw an error
+	if (certificates.length === 0) throw new CertificateValidationError([]);
 
-  const x509certs = certificates.map(c => new X509Certificate(c))
+	// put certificates to ArrayBuffer and run parseCertificate function
+	const x509certs = parseCertificate(
+		stringToArrayBuffer(certificates.join("\n")),
+	);
 
-  // Check dates
-  const now = new Date()
-  const datesValid = x509certs.every(c => new Date(c.validFrom) < now && now < new Date(c.validTo))
-  if (!datesValid) throw new CertificateValidationError(certificates)
+	// Assuming the last certificate is the root
+	const rootCa = x509certs[x509certs.length - 1];
+	const intermediateCa = x509certs[1];
+	const leafCert = x509certs[0];
 
-  // Check that each certificate, except for the last, is issued by the subsequent one.
-  if (certificates.length >= 2) {
-    for (let i = 0; i < x509certs.length - 1; i++) {
-      const subject = x509certs[i]
-      const issuer = x509certs[i + 1]
+	// Validate the certificate chain
+	const chainEngine = new CertificateChainValidationEngine({
+		certs: [rootCa, intermediateCa, leafCert],
+		checkDate: new Date(),
+		trustedCerts: [rootCa],
+	});
 
-      if (subject.checkIssued(issuer) === false || subject.verify(issuer.publicKey) === false) {
-        throw new CertificateValidationError(certificates)
-      }
-    }
-  }
+	const chain = await chainEngine.verify();
 
-  // Ensure that the last certificate in the chain is the expected root CA.
-  if (x509certs[x509certs.length - 1].fingerprint256 !== rootCertFingerprint) {
-    throw new CertificateValidationError(certificates)
-  }
+	// If the chain result is invalid, throw an error
+	if (chain.result === false)
+		throw new CertificateValidationError(certificates);
+
+	// Convert the certificate back to its DER form
+	const rootCaArrayBuffer = rootCa.toSchema(true).toBER(false);
+
+	// Compute and set the fingerprint
+	const hashArray = await computeSHA256(rootCaArrayBuffer);
+	const rootCaFingerprint = bufferToColonSeparatedHex(hashArray);
+
+	// Ensure that the last certificate in the chain is the expected root CA.
+	if (rootCaFingerprint !== rootCertFingerprint) {
+		throw new CertificateValidationError(certificates);
+	}
 }
